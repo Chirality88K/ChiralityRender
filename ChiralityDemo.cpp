74b71afe09497726b2869494c1765668c29c353f
Chirality88K <1239627642@qq.com>
2025-04-28 10:10:15 +0800

    Upload CMakeLists and demo

---
+++ ChiralityDemo.cpp
@@ -0,0 +1,19 @@
+#include "Light.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "Scene.h"
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    //gCamera.Scale(-yoffset * 5.0);
+}
+
+int main(int argc, char **argv) {
+    Scene test_scene("test");
+    if (!test_scene.Initialize())
+    {
+        return -1;
+    }
+    test_scene.BeginRender();
+    return 0;
+}
